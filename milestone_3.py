ac2a3c07b159fa055fa21a42d4c5a2e3d2f6f4a6
WitnessOfThe <puzurdan@gmail.com>
2023-02-08 20:21:13 +0000

    Taks3Mil3

--- milestone_3.py
+++ milestone_3.py
@@ -1,16 +1,19 @@
 import random
-
+def check_guess(guess,word):
+    guess = guess.lower()
+    if guess in word:
+        print(f"Good guess! {guess}")
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")
+def ask_for_input(word):
+    while True:
+        guess = input("Gimmi a letter: ")
+        if len(guess) == 1 and guess.isalpha() == 1: 
+            check_guess(guess,word)
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
 if __name__ == '__main__':
 
     word_list = ['apple', 'banana', 'orange', 'pear', 'strawberry', 'watermelon']
     word      = random.choice(word_list)
-    while True:
-        guess = input("Gimmi a letter: ")
-        if len(guess) == 1 and guess.isalpha() == 1: 
-            if guess in word:
-                print(f"Good guess! {guess}")
-                break
-            else:
-                print(f"Sorry, {guess} is not in the word. Try again.")
-        else:
-            print("Invalid letter. Please, enter a single alphabetical character.")
+    ask_for_input(word)
