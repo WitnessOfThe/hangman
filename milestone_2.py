17e58bcbcb5e1ac290f2acdeeea07c6cf855c6b6
WitnessOfThe <puzurdan@gmail.com>
2023-02-08 19:42:22 +0000

    Task 4

--- milestone_2.py
+++ milestone_2.py
@@ -1,6 +1,15 @@
 import random
+import string
+
 if __name__ == '__main__':
     word_list = ['apple', 'banana', 'orange', 'pear', 'strawberry', 'watermelon']
     word = random.choice(word_list)
     print(word)
-    guess = input("Gimmi a letter: ")    
+    while True:
+        guess = input("Gimmi a letter: ")
+        if len(guess) == 1: 
+            if guess in string.ascii_letters:
+                print( "Good guess!")
+                break
+        else:
+            print("Oops! That is not a valid input.")
