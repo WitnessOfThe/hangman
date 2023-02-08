cc0b3ca34eed89ba2556123ed9e759717fffca66
WitnessOfThe <puzurdan@gmail.com>
2023-02-08 19:31:28 +0000

    Task 3

--- milestone_2.py
+++ milestone_2.py
@@ -3,4 +3,4 @@ if __name__ == '__main__':
     word_list = ['apple', 'banana', 'orange', 'pear', 'strawberry', 'watermelon']
     word = random.choice(word_list)
     print(word)
-    
+    guess = input("Gimmi a letter: ")    
