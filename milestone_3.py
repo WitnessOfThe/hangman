0f8a87cbc5e1f553c6ac7d24fce9fea435eaefba
WitnessOfThe <puzurdan@gmail.com>
2023-02-08 20:41:19 +0000

    Task3Mil3

--- milestone_3.py
+++ milestone_3.py
@@ -3,7 +3,6 @@ def check_guess(guess,word):
     guess = guess.lower()
     if guess in word:
         print(f"Good guess! {guess}")
-        break
     else:
         print(f"Sorry, {guess} is not in the word. Try again.")
 def ask_for_input(word):
@@ -14,7 +13,6 @@ def ask_for_input(word):
         else:
             print("Invalid letter. Please, enter a single alphabetical character.")
 if __name__ == '__main__':
-
     word_list = ['apple', 'banana', 'orange', 'pear', 'strawberry', 'watermelon']
     word      = random.choice(word_list)
     ask_for_input(word)
