6514a698cddae2c16478e13c65a8e78d2e2c7ccd
WitnessOfThe <puzurdan@gmail.com>
2023-02-09 13:10:35 +0000

    Task2Mil4

--- milestone_4.py
+++ milestone_4.py
@@ -7,5 +7,22 @@ class Hangman:
         self.num_lives       = num_lives
         self.num_letters     = len(set(self.word))
         self.list_of_guesses = []
-
-    
+    def check_guess(self,guess):
+        guess = guess.lower()
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+#        else:
+ #           print(f"Sorry, {guess} is not in the word. Try again.")
+    def ask_for_input(self):
+        while True:
+            guess = input("Gimmi a letter: ")
+            if not(len(guess) == 1 and guess.isalpha() == 1): 
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+            else:
+                self.check_guess(guess)
+                
+if __name__ == '__main__':
+    word_list = ['apple', 'banana', 'orange', 'pear', 'strawberry', 'watermelon']
+    Hangman(word_list).ask_for_input()
