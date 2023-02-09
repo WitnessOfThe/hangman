e87c24f6b3c693adf51c034cf4fad0be21096264
WitnessOfThe <puzurdan@gmail.com>
2023-02-09 14:28:56 +0000

    Task2Mil4_help6

--- milestone_4.py
+++ milestone_4.py
@@ -1,6 +1,7 @@
+import random
 class Hangman:
     def __init__(self, word_list,num_lives=5):
-        import random
+        
         self.word_list       = word_list
         self.word            = random.choice(word_list)
         self.word_guessed    = ['_']*len(self.word)
